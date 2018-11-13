51891f94c05106ac53931d36edbc138f37ff2a7d
Flora Ferat <fetahflj@students.zhaw.ch>
2018-11-13 16:31:00 +0100

    bis und mit Kapitel 8.2 durchgeführt

--- src/main/java/ch/zhaw/gpi/twitterreview/delegates/SendTweetDelegate.java
+++ src/main/java/ch/zhaw/gpi/twitterreview/delegates/SendTweetDelegate.java
@@ -1,8 +1,10 @@
 package ch.zhaw.gpi.twitterreview.delegates;
 
+import ch.zhaw.gpi.twitterreview.services.TwitterService;
 import javax.inject.Named;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Implementation des Service Tasks "Tweet senden"
@@ -12,17 +14,23 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 @Named("sendTweetAdapter")
 public class SendTweetDelegate implements JavaDelegate {
 
+    // Variable von Twitterservice
+    @Autowired
+    private TwitterService twitterService;
     /**
-     * 1. Die Prozessvariable tweetContent wird ausgelesen
-     * 2. Dieser Text wird in der Konsole ausgegeben
-     * 
-     * @param execution        Objekt, welches die Verknüpfung zur Process Engine und aktuellen Execution enthält
-     * @throws Exception
-     */
+    * Postet einen Tweet mit dem gewünschten Text
+    *
+    * @param de Objekt, welches die Verknüpfung zur Process Engine und zur aktuellen Execution enthält
+    * @throws Exception
+    */
     @Override
-    public void execute(DelegateExecution execution) throws Exception {
-        String tweetContent = (String) execution.getVariable("tweetContent");
-        System.out.println("!!!!!!!! Folgender Tweet wird veröffentlicht: " + tweetContent);
-    }
-    
+    public void execute(DelegateExecution de) throws Exception {
+        //String tweetContent = (String) execution.getVariable("tweetContent");
+        //System.out.println("!!!!!!!! Folgender Tweet wird veröffentlicht: " + tweetContent);
+        
+        // Prozessvariable tweetContent wird ausgelesen
+        String tweetContent = (String) de.getVariable("tweetContent");
+        // Dieser Text wird dem Twitter Service an die Methode updateStatus übergeben
+        twitterService.updateStatus(tweetContent);
+    }   
 }
